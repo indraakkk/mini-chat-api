bd5cc7e157d335a1cb7107ee9d3ab6d548c916a2
Indra Putra <indrakoslab@gmail.com>
2021-11-24 13:46:32 +0700

    create initial table

---
+++ database/migrations/2021_11_23_094527_create_room_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignIdColumnDefinition;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->index();
+            $table->string('name', 255);
+            $table->string('value', 255);
+            $table->string('user_uuid', 255)->index();
+            $table->timestamps();
+
+            if (Schema::hasTable('users')) {
+                $table->foreign('user_uuid')->references('uuid')->on('users')->onDelete('cascade');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
